fa6146e15d445736ec0f7bec19a08dc072035f7d
numcayX2 <sokulshop5@gmail.com>
2021-10-12 21:59:21 +0700

    Mainweb
    ver 1.2

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function toggle() {
 //navbar scrollmode
 window.onscroll = function() {scrollFunction()};
 
-if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("logo").style.height = "30px";
+      document.getElementById("").style.fontSize = "25px";
+    } else {
+        document.getElementById("logo").style.height = "50px";
+      document.getElementById("").style.fontSize = "25px";
+    }
   }
