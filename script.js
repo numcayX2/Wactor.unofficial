8c290f008b0996f8984cf015314b9ef123b91c63
numcayX2 <sokulshop5@gmail.com>
2021-10-12 21:40:02 +0700

    Mainweb 1

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function toggle() {
     var element = document.getElementById("member-box");
    element.classList.toggle("member-box-active");
 }
+
+//navbar scrollmode
+window.onscroll = function() {scrollFunction()};
+
+if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
