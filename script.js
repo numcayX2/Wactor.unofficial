5c232071fb6d1274d2637ab057e5436604762903
numcayX2 <sokulshop5@gmail.com>
2021-10-13 18:35:21 +0700

    Mainweb
    ver 1.6

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ function toggle() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 80) {
       document.getElementById("logo").style.height = "25px";
     } else {
         document.getElementById("logo").style.height = "50px";
     }
   }
 
-//navber change to smaller
+//navbar change to smaller
+
+//intro -> out
+function introbutton() {
+  var outro = document.getElementById('intro');
+  outro.classList.add('out');
+
+  setTimeout(function(){ outro.style.top = "-100vh"}, 500);
+}
